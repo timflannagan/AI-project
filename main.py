c8a8de3aac90f26f6ff6baf698cd40a46c19d71f
timflannagan1 <timflannagan@gmail.com>
2018-11-05 11:12:26 -0500

    Setup barebones python environment

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ Name: Tim Flannagan
 Date: 10/17/18
 """
 
+import MySQLdb
+
+def main():
+    print('This is currently a stub function')
+
 if __name__ == '__main__':
-    print('true')
+    """
+    Driver for the NBA chatbot
+    """
+    main()
