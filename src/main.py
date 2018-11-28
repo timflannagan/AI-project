e08d309d255e05669da30612c9da55f1c611349d
timflannagan1 <timflannagan@gmail.com>
2018-11-28 16:57:42 -0500

    Add more entries to response lists.

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import random
 import string
 import io
 import nba_py
+import random
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -15,13 +16,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 '''Constants declarations'''
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+FAIL_RESPONSES = ["I am sorry! I don't understand you.", "I wasn't able to understand that last question."]
+END_INPUTS = ['bye', 'exit', 'leave']
 
 '''Function declarations'''
-def LemTokens(tokens):
+def lem_tokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
 
-def LemNormalize(text):
-    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+def lem_normalize(text):
+    return lem_tokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
 def download_nltk_pkgs():
     nltk.download('punkt')
@@ -32,29 +35,45 @@ def greeting(sentence):
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
 
-def response(user_response):
-    robo_response = ''
-    TfidfVec = TfidfVectorizer(tokenizer = LemNormalize, stop_words = 'english')
+def response(user_response, debug = False):
+    TfidfVec = TfidfVectorizer(tokenizer = lem_normalize, stop_words = 'english')
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
     idx = vals.argsort()[0][-2]
     flat = vals.flatten()
     flat.sort()
-    req_tfidf = flat[-2]
 
-    if(req_tfidf == 0):
-        robo_response = robo_response + "I am sorry! I don't understand you"
-        return robo_response
+    req_tfidf = flat[-2]
+    robo_response = ''
+
+    if req_tfidf == 0:
+        index = random.randrange(len(FAIL_RESPONSES))
+        robo_response = robo_response + FAIL_RESPONSES[index]
     else:
         robo_response = robo_response + sent_tokens[idx]
-        return robo_response
 
-'''Driver declaration'''
-if __name__ == '__main__':
-    f = io.open('data/chatbot.txt', mode = 'r', errors = 'ignore', encoding = 'utf-8')
+    if debug:
+        print("Returning '{}'".format(robo_response))
+
+    return robo_response
+
+def chatbot_introduction():
+    print('This is the NBA Chatbot! Enter your question below: ')
+
+def read_chatbot_data(data_dir, data_name, download_pkg = False):
+    f = io.open(data_dir + '/' + data_name, mode = 'r', errors = 'ignore', encoding = 'utf-8')
     raw = f.read()
     raw = raw.lower()
 
+    '''Download nltk packages as needed'''
+    if download_pkg:
+        download_nltk_pkgs()
+
+    return raw
+
+if __name__ == '__main__':
+    '''Driver declaration'''
+    raw = read_chatbot_data('data', 'chatbot.txt')
     sent_tokens = nltk.sent_tokenize(raw)
     word_tokens = nltk.word_tokenize(raw)
     lemmer = nltk.stem.WordNetLemmatizer()
@@ -62,19 +81,19 @@ if __name__ == '__main__':
     remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
     flag = True
 
-    print('This is the NBA Chatbot! Enter your question below: ')
+    chatbot_introduction()
 
-    while(flag == True):
+    while flag:
         user_response = raw_input()
         user_response = user_response.lower()
 
-        if user_response != 'bye':
-            if user_response == 'thanks' or user_response == 'thank you'):
+        if user_response not in END_INPUTS:
+            if user_response == 'thanks' or user_response == 'thank you':
                 flag = False
-                print('NBA Chatbot: You are welcome!')
+                print('> NBA Chatbot: You are welcome!')
             else:
                 if (greeting(user_response) != None):
-                    print("NBA Chatbot: " + greeting(user_response))
+                    print("> NBA Chatbot: " + greeting(user_response))
                 else:
                     sent_tokens.append(user_response)
                     word_tokens = word_tokens + nltk.word_tokenize(user_response)
@@ -83,13 +102,13 @@ if __name__ == '__main__':
                     chatbot_response = response(user_response)
 
                     '''Check if the chatbot did not understand that input'''
-                    if chatbot_response is not 'I am sorry':
-                        print(chatbot_response)
+                    if chatbot_response in FAIL_RESPONSES:
+                        print('[ERROR] NBA Chatbot: {}'.format(chatbot_response))
                     else:
-                        print('NBA Chatbot: {}'.format(chatbot_response))
+                        print('> NBA Chatbot: {}'.format(chatbot_response))
 
                     sent_tokens.remove(user_response)
         else:
             '''Exiting the chatbot here.'''
-            flag = False
-            print("NBA Chatbot: Bye! take care..")
+            print("[EXIT] NBA Chatbot: Bye! take care..")
+            break
